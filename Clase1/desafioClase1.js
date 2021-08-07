fe6f3cbf60e0b3125446b0a3e19ac5ee812e934f
gmaurix <utn.mauri@gmail.com>
2021-08-07 18:52:23 -0300

    Clase 1

---
+++ Clase1/desafioClase1.js
@@ -0,0 +1,22 @@
+
+/* ------------------------ Desafio algoritmo simple ------------------------ */
+//Recibe un valor por prompt y compara si 
+let dato1 =prompt('ingresa un numero');
+let num = parseInt(dato1)
+if(num > 100){
+    alert (`El número ingresado es mayor a 100: ${num}`)
+}else {
+    alert(`El número ingresado, ${num}, es menor que: 100`)
+}
+
+/* 
+function queEs(){
+if(num === 84){
+    alert(`el numero es ${num}`)
+}else{
+    console.log('no es 84: '+ dato2)
+}
+}
+
+queEs()
+ */
